b2194e1a7e779ec04ee523a99ffb7c7809c35e52
shivamjha04 <34072255+shivamjha04@users.noreply.github.com>
2021-05-23 20:46:04 +0530

    Update print.groovy

--- print.groovy
+++ print.groovy
@@ -1,7 +1,8 @@
 def call(){
-                    def inputCSVPath = https://github.com/shivamjha04/Case_study/blob/25c82bac0b44a84ef9a5ee7a8929160bcfd3a844/Input_CaseStudy.xlsm
-                    def csvContent = readFile "${inputCSVPath}"
+//                     def inputCSVPath = https://github.com/shivamjha04/Case_study/blob/25c82bac0b44a84ef9a5ee7a8929160bcfd3a844/Input_CaseStudy.xlsm
+//                     def csvContent = readFile "${inputCSVPath}"
                     
-                   sh "echo CSV file path is : ${inputCSVPath}"
-                   sh "echo CSV content is: ${csvContent}"
+//                    sh "echo CSV file path is : ${inputCSVPath}"
+//                    sh "echo CSV content is: ${csvContent}"
+    sh "echo Hello Wordl"
 }
