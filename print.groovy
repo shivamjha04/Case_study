be4215c500d16094d6254404e64111cb9c5e5978
shivamjha04 <34072255+shivamjha04@users.noreply.github.com>
2021-05-23 21:02:55 +0530

    Delete print.groovy

--- print.groovy
+++
@@ -1,8 +0,0 @@
-def call(){
-//                     def inputCSVPath = https://github.com/shivamjha04/Case_study/blob/25c82bac0b44a84ef9a5ee7a8929160bcfd3a844/Input_CaseStudy.xlsm
-//                     def csvContent = readFile "${inputCSVPath}"
-                    
-//                    sh "echo CSV file path is : ${inputCSVPath}"
-//                    sh "echo CSV content is: ${csvContent}"
-    sh "echo Hello Wordl"
-}
