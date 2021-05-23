cb883b65d67a627e8207ee6dd6e78752e71abc58
shivamjha04 <34072255+shivamjha04@users.noreply.github.com>
2021-05-23 20:42:32 +0530

    Update print.groovy

--- print.groovy
+++ print.groovy
@@ -1,5 +1,5 @@
 def call(){
-                    def inputCSVPath = 
+                    def inputCSVPath = https://github.com/shivamjha04/Case_study/blob/25c82bac0b44a84ef9a5ee7a8929160bcfd3a844/Input_CaseStudy.xlsm
                     def csvContent = readFile "${inputCSVPath}"
                     
                    sh "echo CSV file path is : ${inputCSVPath}"
