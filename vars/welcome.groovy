9ab0dd0f15bdcd0966333cea0520d139c991a4bd
shivamjha04 <34072255+shivamjha04@users.noreply.github.com>
2021-05-23 21:03:59 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -2,7 +2,7 @@ def call(){
 //                     def inputCSVPath = https://github.com/shivamjha04/Case_study/blob/25c82bac0b44a84ef9a5ee7a8929160bcfd3a844/Input_CaseStudy.xlsm
 //                     def csvContent = readFile "${inputCSVPath}"
                     
-//                    sh "echo CSV file path is : ${inputCSVPath}"
-//                    sh "echo CSV content is: ${csvContent}"
-    sh "echo Hello Wordl"
+//                     echo "CSV file path is : ${inputCSVPath}"
+//                     echo "CSV content is: ${csvContent}"
+    echo "Hello Wordl"
 }
