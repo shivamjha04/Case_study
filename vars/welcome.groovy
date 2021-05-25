05200ac1a6a9a3a1d376b1f9bd9f3e5c6676ecd3
shivamjha04 <34072255+shivamjha04@users.noreply.github.com>
2021-05-25 17:16:04 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,5 +1,9 @@
 def call(){
+    try{
     echo "Hello World"
     emailext attachLog: true, body: 'Hi,', subject: 'Jenkins report', to: 'shivamjha04@gmail.com'
+    }
+    catch(err){
+        emailext body: "${err}", subject:'Jenkins file', to:'shivamjha04@gmail.com'
      
 }
