c98c3563ccdb7f740f4443b26e9b4aaf35797b92
shivamjha04 <34072255+shivamjha04@users.noreply.github.com>
2021-05-26 14:04:29 +0530

    Update CSVread.groovy

--- vars/CSVread.groovy
+++ vars/CSVread.groovy
@@ -1,1 +1,9 @@
-def call()
+def call(){
+   
+      def inputCSVPath = input message: 'Upload file', parameters: [file(name: 'Test.csv', description: 'Upload only CSV file')]            
+      def csvContent = readFile "${inputCSVPath}"
+                        
+      echo ("CSV FILE PATH IS : ${inputCSVPath}")
+      echo ("CSV CONTENT IS: ${csvContent}") 
+  
+}
