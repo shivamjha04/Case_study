2d8cc4efc69a9278690c1f42ad805ff61e3fbb7f
shivamjha04 <34072255+shivamjha04@users.noreply.github.com>
2021-05-26 14:08:47 +0530

    Update CSVread.groovy

--- vars/CSVread.groovy
+++ vars/CSVread.groovy
@@ -3,7 +3,8 @@ def call(){
       def inputCSVPath = input message: 'Upload file', parameters: [file(name: 'Test.csv', description: 'Upload only CSV file')]            
       def csvContent = readFile "${inputCSVPath}"
                         
-      echo ("CSV FILE PATH IS : ${inputCSVPath}")
-      echo ("CSV CONTENT IS: ${csvContent}") 
-  
+      echo "CSV FILE PATH IS : ${inputCSVPath}"
+      echo "CSV CONTENT IS: ${csvContent}"
+      echo "Sending Email"
+      emailext attachLog: true, body: 'Hi,', subject: 'Jenkins report Successful', to: 'shivamjha04@gmail.com'
 }
